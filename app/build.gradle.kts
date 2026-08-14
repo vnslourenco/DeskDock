ebbe4c4662aec393dc7eb15cd68ab8db4bf1bdd0
Vinicius Lourenço <75282762+vnslourenco@users.noreply.github.com>
2026-08-14 18:29:53 -0300

    Release DeskDock v0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.deskdock.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
+        versionCode = 7
         versionName = "0.5.0"
     }
 
