f868927836503f2249532faef121965e8bdc3c60
Vinicius Lourenço <75282762+vnslourenco@users.noreply.github.com>
2026-08-20 02:34:17 -0300

    Bump DeskDock to 2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.deskdock.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 23
-        versionName = "2.0.0"
+        versionCode = 24
+        versionName = "2.1.0"
     }
 
     buildTypes {
