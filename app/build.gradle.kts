a494702fc2a9a4c42b92864de5f6862f31d59120
Vinicius Lourenço <75282762+vnslourenco@users.noreply.github.com>
2026-08-20 02:02:45 -0300

    Bump DeskDock to 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.deskdock.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "1.3.1"
+        versionCode = 22
+        versionName = "1.4.0"
     }
 
     buildTypes {
