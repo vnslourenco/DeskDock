d6325212a5557ef6609a047c7c6d6f8e49fdca1b
Vinicius Lourenço <75282762+vnslourenco@users.noreply.github.com>
2026-08-21 15:42:35 -0300

    Build DeskDock 2.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.deskdock.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 25
-        versionName = "2.2.0"
+        versionCode = 30
+        versionName = "2.3.3"
     }
 
     buildTypes {
